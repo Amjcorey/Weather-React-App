454d1c783db3eda709b974372c35b00550b38c70
Amjcorey <76081915+Amjcorey@users.noreply.github.com>
2021-11-02 09:14:00 -0700

    created app and began changing css and js

---
+++ weather_app/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React Weather App</h1>
+        <main> </main>
+        <footer>Page created by Amanda Corey</footer>
+      </header>
+    </div>
+  );
+}
+
+export default App;

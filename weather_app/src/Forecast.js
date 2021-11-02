8b3bc320b0e436bc3a74a092738251907640e024
Amjcorey <76081915+Amjcorey@users.noreply.github.com>
2021-11-02 12:03:37 -0700

    Update Forecast.js

--- weather_app/src/Forecast.js
+++ weather_app/src/Forecast.js
@@ -11,32 +11,47 @@ export default function getForecast(props) {
   //  const [description, setDescription] = useState(null);
   //  const [icon, setIcon] = useState(null);
 
-    function handleResponse(response) {
-     // event.preventDefault();
-      alert(`The weather in ${response.data.name} is ${response.data.main.temp}`);
+    function handleSubmit(event) {
+      event.preventDefault();
+     // alert(`The weather in ${response.data.name} is ${response.data.main.temp}`);
+        
+        let apiKey = "70de72ce25d0801c193edd1d17ced422";
+        let units = "metric";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units}`;
+        
+        axios.get(apiUrl).then(displayWeather);
+        
     }
 
-    let apiKey = "70de72ce25d0801c193edd1d17ced422";
-    let units = "metric";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units}`;
-        
-    axios.get(apiUrl).then(handleResponse);
+
+    function displayWeather(response) {
+        setTemperature(Math.round(response.data.main.temp));
+        setDescription(response.data.weather[0].description);
+        setWind(Math.round(response.data.wind.speed));
+        setHumidity(response.data.main.humidity);
+        setIcon(
+      `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  }
 
     return (
         <div className="container">
-            <form className="form" >
+            <form className="form" onSubmit="handleSubmit">
                 <input
                     type="search"
                     placeholder="Enter a city"
                     className="form-control"
-                    
+                    onChange={displayCity}
                 />
 
                 <input
-                    type="button" value="Search" className="btn-primary"
+                    type="button" value="Search" className="btn btn-primary"
                 />
             </form>
 
+            <div>
+
+            </div>
         </div>
     );
 
